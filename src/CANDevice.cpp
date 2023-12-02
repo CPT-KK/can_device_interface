674db18b5d3eabe065c384d8d06242abba8201f9
Cptkkk <cptkk@qq.com>
2023-12-02 16:55:21 +0800

    now work!

--- src/CANDevice.cpp
+++ src/CANDevice.cpp
@@ -13,10 +13,20 @@
 #include <thread>
 
 class CANDevice {
-   public:
-    CANDevice(unsigned int id, bool isRead, int dlc = 8, std::function<void(const struct can_frame&)> callback = nullptr)
-        : id_(id), isRead_(isRead), dlc_(dlc), callback_(callback), stopThread_(false) {
-        initSocket();
+public:
+    // Constructor for read-only CAN device
+    CANDevice(const char* interface, unsigned int readId, std::function<void(const struct can_frame&)> callback) : readId_(readId), writeId_(0), callback_(callback), stopThread_(false), canRead_(true), canWrite_(false) {
+        initSocket(interface);
+    }
+
+    // Constructor for write-only CAN device
+    CANDevice(const char* interface, unsigned int writeId) : readId_(0), writeId_(writeId), callback_(nullptr), stopThread_(false), canRead_(false), canWrite_(true) {
+        initSocket(interface);
+    }
+
+    // Constructor for read-write CAN device
+    CANDevice(const char* interface, unsigned int readId, std::function<void(const struct can_frame&)> callback, unsigned int writeId) : readId_(readId), writeId_(writeId), callback_(callback), stopThread_(false), canRead_(true), canWrite_(true) {
+        initSocket(interface);
     }
 
     ~CANDevice() {
@@ -28,11 +38,13 @@ class CANDevice {
     }
 
     void read() {
-        if (isRead_) {
+        if (canRead_) {
+            // Check if callback function is set
             if (!callback_) {
                 throw std::logic_error("Callback function is not set for reading device.");
             }
 
+            // Start receive thread
             receiveThread_ = std::thread([this]() {
                 struct can_frame frame;
                 while (!stopThread_) {
@@ -45,7 +57,7 @@ class CANDevice {
                         // Connection closed
                         throw std::runtime_error("Connection closed by peer");
                     } else if (bytesRead == sizeof(struct can_frame)) {
-                        if (frame.can_id == id_) {
+                        if (frame.can_id == readId_) {
                             callback_(frame);
                         }
                     }
@@ -56,26 +68,44 @@ class CANDevice {
         }
     }
 
-    void send(const void* payload) {
-        struct can_frame frame;
-        frame.can_dlc = dlc_;
-        std::memcpy(frame.data, payload, std::min(static_cast<size_t>(dlc_), sizeof(frame.data)));
+    void send(const unsigned char* payload, int dlc) {
+        if (canWrite_) {
+            // Declare frame to send
+            struct can_frame frame;
 
-        if (id_ <= CAN_SFF_MASK) {
-            // Standard ID
-            frame.can_id = id_;
+            // Set data length
+            frame.can_dlc = dlc;
+
+            // Check if payload size is valid
+            if (dlc > sizeof(frame.data)) {
+                throw std::length_error("Payload size exceeds the maximum allowed size.");
+            }
+
+            // Copy payload
+            std::memcpy(frame.data, payload, dlc);
+
+            // Set ID, and see if we need to set the EFF flag
+            if (writeId_ <= CAN_SFF_MASK) {
+                // Standard ID
+                frame.can_id = writeId_;
+            } else {
+                // Extended ID
+                frame.can_id = writeId_ | CAN_EFF_FLAG;
+            }
+
+            // Send frame
+            ssize_t bytesSent = write(socket_, &frame, sizeof(struct can_frame));
+
+            // Check if all bytes were sent
+            if (bytesSent == -1) {
+                throw std::runtime_error(std::string("Error sending data frame: ") + strerror(errno));
+            }
         } else {
-            // Extended ID
-            frame.can_id = id_ | CAN_EFF_FLAG;
-        }
-
-        ssize_t bytesSent = write(socket_, &frame, sizeof(struct can_frame));
-        if (bytesSent == -1) {
-            throw std::runtime_error(std::string("Error sending data frame: ") + strerror(errno));
+            throw std::logic_error("This device is not configured for writing.");
         }
     }
 
-    void initSocket() {
+    void initSocket(const char* interface) {
         socket_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
         if (socket_ == -1) {
             throw std::runtime_error(std::string("Error creating socket: ") + strerror(errno));
@@ -84,7 +114,7 @@ class CANDevice {
         struct sockaddr_can addr;
         struct ifreq ifr;
 
-        std::strcpy(ifr.ifr_name, "can0");  // Replace "can0" with the actual interface name
+        std::strcpy(ifr.ifr_name, interface);
         if (ioctl(socket_, SIOCGIFINDEX, &ifr) == -1) {
             close(socket_);
             throw std::runtime_error(std::string("Error getting interface index: ") + strerror(errno));
@@ -101,13 +131,13 @@ class CANDevice {
         // 设置过滤器
         struct can_filter filter[1];
 
-        if (id_ <= CAN_SFF_MASK) {
+        if (readId_ <= CAN_SFF_MASK) {
             // Standard ID
-            filter[0].can_id = id_;
+            filter[0].can_id = readId_;
             filter[0].can_mask = CAN_SFF_MASK;
         } else {
             // Extended ID
-            filter[0].can_id = id_;
+            filter[0].can_id = readId_;
             filter[0].can_mask = CAN_EFF_MASK;
         }
 
@@ -117,59 +147,40 @@ class CANDevice {
         }
     }
 
-   private:
-    unsigned int id_;
-    int dlc_;  // 载荷长度
-    int socket_;
-    bool isRead_ = true;  // 设备是否配置为读操作
+private:
+    unsigned int readId_;
+    unsigned int writeId_;
     std::function<void(const struct can_frame&)> callback_;
+    int socket_;
     std::thread receiveThread_;
     bool stopThread_;
+    bool canRead_;
+    bool canWrite_;
 };
 
 void mycallback(const struct can_frame& frame) {
-    std::cout << "CAN ID: " << frame.can_id << std::endl;
+    printf("%X[%d]#%s\n", frame.can_id, frame.can_dlc, frame.data);
 }
 
 int main() {
     try {
-        int canId;
-        std::cout << "Enter CAN device ID: ";
-        std::cin >> canId;
+        // Read test
+        CANDevice readDevice("vcan0", 0x301u, mycallback);
+        readDevice.read();
 
-        bool isRead;
-        std::cout << "Enter device property (0 for write, 1 for read): ";
-        std::cin >> isRead;
-
-        if (isRead) {
-            // 如果是读设备，获取回调函数句柄或指针
-            std::cout << "Enter callback function handle or pointer (0 for no callback): ";
-            int callbackHandle;
-            std::cin >> callbackHandle;
-
-            // 创建CANDevice对象并配置为读操作
-            CANDevice canDevice(canId, true, 8, mycallback);
-
-            // 读取数据
-            canDevice.read();
-        } else {
-            // 创建CANDevice对象并配置为写操作
-            CANDevice canDevice(canId, false, 8);
-
-            // 准备要发送的数据
-            std::string payload;
-            std::cout << "Enter payload for CAN frame: ";
-            std::cin >> payload;
-
-            // 发送数据
-            canDevice.send(payload.c_str());
-        }
-
-        // 在此处可以添加其他业务逻辑
+        // Write test
+        CANDevice writeDevice("vcan0", 0x401u);
+        unsigned char payload[8];
+        payload[0] = 0xAA;
+        payload[1] = 0xBB;
+        payload[2] = 0xCC;
+        payload[3] = 0xDD;
+        writeDevice.send(payload, sizeof(payload));
 
         // 主线程会等待用户输入，直到用户按下 Enter 键
-        std::cout << "Press Enter to exit." << std::endl;
-        std::cin.ignore();
+        std::cout << "Press Ctrl + C to exit." << std::endl;
+        std::cin.get();
+
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
